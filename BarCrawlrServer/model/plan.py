e4d578a0fd518e6ccc84bb474362b4750c699173
Alexander Bean Apmann <caboose200@gmail.com>
2017-01-16 02:34:05 -0500

    And here we go

---
+++ BarCrawlrServer/model/plan.py
@@ -0,0 +1,24 @@
+import json
+
+
+class plan:
+
+
+    def __init__(self,thePlan):
+        
+        Plan = json.load(thePlan)
+
+        self.name = Plan["Name"]
+
+        self.address = Plan["Address"]
+
+        self.lat = Plan["Location"][0]
+
+        self.lon = Plan["Location"][1]
+
+        self.note = Plan["Note"]
+
+    def jsonify(self):
+
+        return ""
+        
