dd74d36c0102cf32eab72a7f402c3dd7538c191e
Joe L <jlistro@gmail.com>
2017-01-27 12:12:39 -0500

    save work from class
    it's buggy

--- BarCrawlrServer/bcserver.py
+++ BarCrawlrServer/bcserver.py
@@ -1,15 +1,15 @@
-from flask import Flask, jsonify, current_app
+from flask import Flask, jsonify, current_app, abort, make_response
 
 from BarCrawlrServer.model.plan import plan
 
 server = Flask(__name__)
 
-plans = \
+plans = [
     {
-        'id':0,
+        'id': 0,
         'plan': plan('{"Name":"test_plan_1","Address":"Oceanic location","Location":[0,0],"Note":"Test plan for testing"}')
     }
-
+]
 
 @server.route('/')
 def index():
@@ -17,21 +17,25 @@ def index():
 
 @server.route('/plan', methods=['POST'])
 def create_plan():
-    if not request.json or not 'name' in request.json:
+    if not "Name" in request.json:
         abort(400)
     plans.append(plan(request.json))
     return len(plans)
     
-@server.route('/plan', methods=['GET'])
+@server.route('/plans', methods=['GET'])
 def get_all_plans():
     return jsonify({'plan 1': plans['plan'].jsonify()})
-#def get_plan(plan_id):
-#    p = [plan for plan in plans if plans['id'] == plan_id]
-#    if len(plans) == 0:
-#        abort(400)
-#    if len(plan) == 0:
-#        abort(400)
-#    return plans[plan_id].jsonify()
+
+@server.route('/plan/<int:plan_id>',methods=['GET'])
+def get_plan(plan_id):
+    p = [plan for plan in plans if plan['id'] == plan_id]
+    if len(p) == 0:
+        abort(400)
+    return p[plan_id].plan.jsonify()
+
+@server.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     server.run(debug=True)
