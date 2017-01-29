587a6940323c2b579048d0da491069e70b13a7bd
Joe L <jlistro@gmail.com>
2017-01-29 18:47:36 -0500

    create_plan() fixed

--- BarCrawlrServer/bcserver.py
+++ BarCrawlrServer/bcserver.py
@@ -12,23 +12,19 @@ plans = [
 def index():
     return "Welcome to Bar Crawlr Server"
 
+# Post a new plan to the plans list
 @server.route('/plan', methods=['POST'])
 def create_plan():
-    if not request.json or not 'name' in request.json:
+    if not request.json or not 'Name' in request.json:
         abort(400)
     plans.append(plan(request.json))
-    return len(plans)
-    
+    return jsonify({'plan': plan}), 201
+
+# Get all the plans from the plans list
 @server.route('/plan', methods=['GET'])
 def get_all_plans():
     return jsonify({'plan 1': plans['plan'].jsonify()})
-#def get_plan(plan_id):
-#    p = [plan for plan in plans if plans['id'] == plan_id]
-#    if len(plans) == 0:
-#        abort(400)
-#    if len(plan) == 0:
-#        abort(400)
-#    return plans[plan_id].jsonify()
+
 
 if __name__ == '__main__':
     server.run(debug=True)
