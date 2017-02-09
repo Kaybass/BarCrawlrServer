b6623b8dce91e10e30c63d46ef158596b7b513af
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-09 16:12:33 -0500

    stuff

---
+++ BarCrawlrServer/utilities/barJsons.py
@@ -0,0 +1,30 @@
+from BarCrawlrServer.model.user import user
+from BarCrawlrServer.model.plan import plan
+
+
+def createUsersJsonFromDict(users):
+    json = '{"users":['
+
+    i = 0
+    for key, User in users.items():
+        i += 1
+        if i < len(users.items()):
+            json += User.jsonify() + ','
+        else:
+            json += User.jsonify()
+
+    json += ']}'
+
+    return json
+
+def createGetPlanJson(aPlan,users):
+
+    json = '{"plan":"'
+
+    json += aPlan.jsonify()
+
+    json += ',"users":' + createUsersJsonFromDict(users)
+
+    json += '}'
+
+    return json
