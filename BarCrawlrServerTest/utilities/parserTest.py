737bac5352a93708e0f3f2f146dc09c62e5a64ab
Joe Listro <jlistro@gmail.com>
2017-04-07 00:10:45 -0400

    Added parserTest.py

---
+++ BarCrawlrServerTest/utilities/parserTest.py
@@ -0,0 +1,28 @@
+import unittest
+
+from BarCrawlrServer.utilities.parser import Parser
+from BarCrawlrServer.utilities.logger import Logger
+from BarCrawlrServer.model.event import event, eventCodes, events, eventsTypes
+
+
+#########################################
+# A parser to interpret the data from 
+#     the logger for the graph.
+#########################################
+class Test_parserTest(unittest.TestCase):
+    def test_A(self):
+        evnt = event(eventsTypes[eventCodes.successful_plan_creation]\
+                     ,events[eventCodes.successful_plan_creation].format("kek"))
+
+        logger = Logger("kek.txt")
+
+        logger.log(evnt)
+        
+        prsr = Parser(logger)
+
+
+        #self.assertEquals(logger.getLog()[len(logger.getLog()) - 1].replace('\n',''),evnt.toString())
+        self.assertEquals(Prsr.parse(), Parser.parse(evnt.toString()))
+
+if __name__ == '__main__':
+    unittest.main()
